ac2042a5f965c65c8eee09050af1a282abe2e62f
chen-1580 <chen-1580@163.com>
2016-06-21 00:57:57 +0800

    修改加密规则

--- src/main/java/com/simple/admin/util/MD5Util.java
+++ src/main/java/com/simple/admin/util/MD5Util.java
@@ -21,23 +21,33 @@ public class MD5Util {
 		return hexDigits[d1] + hexDigits[d2];
 	}
 
+	/** 加密规则： 
+	 * 按照被加密字符串和盐值字符串的字节码拼接的字符串做加密
+	 * @param origin 被加密字符串
+	 * @param charsetname 加密盐值
+	 * @return
+	 */
 	public static String MD5Encode(String origin, String charsetname) {
 		String resultString = null;
 		try {
 			resultString = new String(origin);
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			if (charsetname == null || "".equals(charsetname))
-				resultString = byteArrayToHexString(md.digest(resultString
-						.getBytes()));
+				resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
 			else
-				resultString = byteArrayToHexString(md.digest(resultString
-						.getBytes(charsetname)));
-		} catch (Exception exception) {
+				resultString = byteArrayToHexString(md.digest((resultString.getBytes().toString() + charsetname.getBytes().toString()).getBytes()));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return resultString;
 	}
 
 	private static final String hexDigits[] = { "0", "1", "2", "3", "4", "5",
 			"6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+	
+	public static void main(String[] args) {
+		String a = MD5Util.MD5Encode("admin", "123");
+		System.out.println(a);
+	}
 
 }
