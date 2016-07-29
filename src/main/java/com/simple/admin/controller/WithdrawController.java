24179b2e4a51e56136c988deecd43a07df4e95cd
zhengfy1 <zhengfy1@lenovo.com>
2016-07-29 12:26:35 +0800

    update

--- src/main/java/com/simple/admin/controller/WithdrawController.java
+++ src/main/java/com/simple/admin/controller/WithdrawController.java
@@ -1,5 +1,7 @@
 package com.simple.admin.controller;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,11 +12,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
+import com.ruanwei.tool.SmsClient;
 import com.simple.admin.util.AjaxWebUtil;
 import com.simple.admin.util.LoginUserUtil;
+import com.simple.common.util.PrimaryKeyUtil;
+import com.simple.constant.Constant;
 import com.simple.model.Account;
 import com.simple.model.Bank;
 import com.simple.model.User;
@@ -31,7 +37,7 @@ public class WithdrawController {
 	@Autowired
 	private BaseService baseService;
 	
-	@RequestMapping("account")
+	@RequestMapping(value = "account",method=RequestMethod.GET)
 	@ResponseBody
 	public String account(HttpServletRequest request, HttpServletResponse response){
 		User user = LoginUserUtil.getCurrentUser(request);
@@ -42,22 +48,63 @@ public class WithdrawController {
 		return AjaxWebUtil.sendAjaxResponse(request, response, true, "查找成功", JSON.toJSONString(data));
 	}
 	
-	@RequestMapping("doAccount")
+	@RequestMapping(value = "doAccount",method=RequestMethod.POST)
 	@ResponseBody
 	public String doAccount(HttpServletRequest request, HttpServletResponse response){
-		String prmBankId = AjaxWebUtil.getRequestParameter(request,"bankId");
-		String prmReanlName = AjaxWebUtil.getRequestParameter(request,"realName");
-		String prmBankAccount = AjaxWebUtil.getRequestParameter(request,"bankAccount");
-		String prmBankAccountNo = AjaxWebUtil.getRequestParameter(request,"bankAccountNo");
-		Account account = new Account();
-		account.setBankId(Integer.parseInt(prmBankId));
-		account.setRealName(prmReanlName);
-		account.setBankAccount(prmBankAccount);
-		account.setBankAccountNo(Integer.parseInt(prmBankAccountNo));
-		boolean isAccount = withdrawService.addAccount(account);
-		if(isAccount){
+		try {
+			String bankCode = AjaxWebUtil.getRequestParameter(request,"bankCode");
+			String prmReanlName = AjaxWebUtil.getRequestParameter(request,"realName");
+			String prmBankAccount = AjaxWebUtil.getRequestParameter(request,"bankAccount");
+			String prmBankAccountNo = AjaxWebUtil.getRequestParameter(request,"bankAccountNo");
+			String bankPhone = AjaxWebUtil.getRequestParameter(request,"phone");
+			User user = LoginUserUtil.getCurrentUser(request);
+			Account account = new Account();
+			account.setId(PrimaryKeyUtil.getUUID());
+			account.setBankCode(bankCode);
+			account.setRealName(prmReanlName);
+			account.setBankAccount(prmBankAccount);
+			account.setBankAccountNo(prmBankAccountNo);
+			account.setUserPhone(user.getUserPhone());
+			account.setBankPhone(bankPhone);
+			account.setCashTime(new Timestamp(new Date().getTime()));
+			account.setCashAmount(user.getBalance());
+			account.setWeiChatNo(user.getWeChatNo());
+			account.setStatus(Constant.CASH_STATUS_COMMIT);
+			withdrawService.addAccount(account);
+			SmsClient.sendMsg(Constant.ADMIN_PHONE, "提现申请:[申请人："+user.getUserPhone()+"(微信号："+user.getWeChatNo()+"),申请金额："+user.getBalance()+"].");
 			return AjaxWebUtil.sendAjaxResponse(request, response, true, "提现成功", null);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return AjaxWebUtil.sendAjaxResponse(request, response, false, "提现失败："+e.getLocalizedMessage(), null);
 		}
-		return AjaxWebUtil.sendAjaxResponse(request, response, false, "提现失败", null);
+		
+	}
+	
+	@RequestMapping(value = "allowAccount",method=RequestMethod.POST)
+	@ResponseBody
+	public String allowAccount(String id,String remark,HttpServletRequest request, HttpServletResponse response){
+		try {
+			Account a = withdrawService.updateAccountFinised(id, remark);
+			SmsClient.sendMsg(a.getUserPhone(), "提现:"+a.getCashAmount()+"成功.");
+			return AjaxWebUtil.sendAjaxResponse(request, response, true, "提现成功", null);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return AjaxWebUtil.sendAjaxResponse(request, response, false, "提现失败："+e.getLocalizedMessage(), null);
+		}
+		
+	}
+	
+	@RequestMapping(value = "cancelAccount",method=RequestMethod.POST)
+	@ResponseBody
+	public String cancelAccount(String id,String remark,HttpServletRequest request, HttpServletResponse response){
+		try {
+			Account a = withdrawService.updateAccountCancel(id, remark);
+			SmsClient.sendMsg(a.getUserPhone(), "提现取消:"+a.getCashAmount()+"成功.");
+			return AjaxWebUtil.sendAjaxResponse(request, response, true, "提现成功", null);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return AjaxWebUtil.sendAjaxResponse(request, response, false, "提现失败："+e.getLocalizedMessage(), null);
+		}
+		
 	}
 }
